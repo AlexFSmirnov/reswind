a67823bb60ae5020fd1ada839a6323050c29ce10
AlexFSmirnov <alex.f.smirnov@yandex.ru>
2016-07-08 17:26:32 +0300

    Updated example

--- example.py
+++ example.py
@@ -1,30 +1,29 @@
 import pygame
 from pygame import *
 import reswind
+import sys
 
 
 pygame.init()
-
-
 window = reswind.ResizableWindow((300, 300), (300, 300),
-                         gap_fill_type="gradient_down",
-                         gap_fill_color=(0, 255, 0),
-                         gap_steps=100,
-                         draw_lines=False)
+                                 gap_fill_type  = "gradient_up",
+                                 gap_fill_color = (0, 0, 0),
+                                 gap_steps      = 100,
+                                 draw_lines     = True,
+                                 lines_color    = (100, 100, 100),
+                                 smoothscale    = False)
 
-window.main_surface.fill((0,255,0))
+window.main_surface.fill((0, 255, 0))
+draw.circle(window.main_surface, (0, 0, 0), (150, 150), 100)
 
 while True:   
     e = pygame.event.wait()
     if e.type == pygame.QUIT:
         pygame.quit()
+        sys.exit(0)
     
     if e.type == VIDEORESIZE:
-        window.updateSize(e.dict['size'])
-        
-    if e.type == KEYDOWN and e.key == K_w: 
-        print(pygame.display.Info())
-        
+        window.updateSize(e.dict['size'])        
     
     window.update()
     
